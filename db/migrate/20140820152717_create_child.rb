6aaa202efdc995de57bc0ea21ad6ef948d0279ad
Ben White <whtouche@gmail.com>
2014-08-20 11:54:51 -0400

    Create Child table referencing users

---
+++ db/migrate/20140820152717_create_child.rb
@@ -0,0 +1,15 @@
+class CreateChild < ActiveRecord::Migration
+  def change
+    create_table :child do |t|
+      t.string :first_name, null: false
+      t.string :last_name
+      t.date :dob, null: false
+      t.string   :avatar
+      t.integer :donations_number
+      t.decimal :donations_total
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
